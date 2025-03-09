098fad2334b9849e7205beb4c855cd88312ef0e3
Houssien Zeineddine <houssienzd@hotmail.com>
2025-03-09 04:05:12 +0200

    writing signin api and testing it successfully

--- server/apis/v1/index.php
+++ server/apis/v1/index.php
@@ -4,13 +4,20 @@
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    $conn = require '../config/connection.php';
-    require '../../models/user';
+    /* $conn = require '../config/connection.php'; */
+    $conn = require 'C:/xampp/htdocs/article_FAQs/server/config/connection.php';
+    require '../../models/user.php';
+    require '../../models/userSkeleton.php';
 
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    $user = userSkeleton::createUserSkeleton(null, $email, $password);
     $result = User::signIn($conn, $email, $password);
+
+    /* if($result['status'] === 'success') {
+        
+    } */
 
     echo json_encode($result);
 
